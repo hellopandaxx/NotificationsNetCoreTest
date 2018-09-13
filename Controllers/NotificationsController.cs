1358f8b25f8bff93937e2d942f75a5ca244e1b67
Sergey Kuznetsov <skuznetsov@intermedia.net>
2018-09-13 11:09:38 +0300

    Test Notification Service

---
+++ Controllers/NotificationsController.cs
@@ -0,0 +1,35 @@
+﻿namespace TestNotificationsService.Controllers
+{
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Options;
+
+    [Route("api/notifications")]
+    public class NotificationsController : Controller
+    {
+        private readonly IOptions<PushoverConfiguration> config;
+
+        private HttpClient httpClient = new HttpClient();
+
+        public NotificationsController(IOptions<PushoverConfiguration> config)
+        {
+            this.config = config;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<string> Send([FromBody]string message)
+        {
+            var values = new Dictionary<string, string> { { "token", this.config.Value.Token }, { "user", this.config.Value.User }, { "message", message } };
+
+            var content = new FormUrlEncodedContent(values);
+
+            var response = await this.httpClient.PostAsync("https://api.pushover.net/1/messages.json", content);
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ PushoverConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace TestNotificationsService
+{
+    public class PushoverConfiguration
+    {
+        public string Token { get; set; }
+        public string User { get; set; }
+    }
+}
